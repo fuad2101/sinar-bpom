a7d6897c0f21698ae81f1a3e021ddd86a57ca7a0
BPOM <fuady.teknikinformatika@gmail.com>
2026-08-26 14:50:50 +0800

    first commit

---
+++ resources/views/pages/auth/login.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.auth')
+
+@section('content')
+
+<form action="" method="POST">
+    <label for="">
+        Username
+        <input type="text" name="username" id="">
+    </label>
+    <label for="">
+        Password
+        <input type="password" name="password" id="">
+    </label>
+</form>
+
+<div class="flex bg-amber-700">
+    <p>test</p>
+</div>
+@endsection
