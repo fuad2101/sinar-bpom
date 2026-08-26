823db1cf0d7acbb2fa90c958f5c911e581e5fa42
Fuad <muhfuad67@gmail.com>
2026-08-27 04:58:15 +0800

    modify env.example,add post route for login form

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/login', function () {
     return view('pages.auth.login');
 });
+
+Route::post('/login', function () {
+    return view('pages.auth.login');
+});
+
 Route::get('/register', function () {
     return view('pages.auth.register');
 });
